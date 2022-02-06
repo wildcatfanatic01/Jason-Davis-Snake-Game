508ce68bc578fdc2ea590a36c48508e9d249a2c2
Jason Davis <jason_davis@mymail.eku.edu>
2022-02-06 17:16:44 -0500

    Edited comments in Settings.cs, Circle.cs, and Form1.cs file

--- Circle.cs
+++ Circle.cs
@@ -8,10 +8,10 @@ namespace SnakeGame
 {
     class Circle
     {
-        public int X { get; set; }      //This is a public int class called X.
-        public int Y { get; set; }      //This is a public int class called Y.
+        public int X { get; set; }      //This will identify the X coordinate location of the circle.
+        public int Y { get; set; }      //This will identify the Y coordinate location of the circle.
 
-        public Circle()                 //This function will reset the X and Y to 0.
+        public Circle()                 //This function will reset the X and Y coordinate locations to 0, i.e. the Origin.
         {
             X = 0;
             Y = 0;
--- Settings.cs
+++ Settings.cs
@@ -9,8 +9,8 @@ namespace SnakeGame
     class Settings
     {
 
-        public static int Width { get; set; }       //This sets the Width property.
-        public static int Height { get; set; }      //This sets the Height property.
+        public static int Width { get; set; }       //This sets the Width property for the window of the game.
+        public static int Height { get; set; }      //This sets the Height property for the window of the game.
 
         public static string directions;            //This will be used to set the direction.
 
